1238967ae9e96b39b6510ba3eef87e1502ad7eed
Daniel Stroe <stroe.daniel@yahoo.com>
2013-12-23 07:56:48 +0200

    no message

---
+++ Conditionals, Loops/Ex32.dpr
@@ -0,0 +1,24 @@
+program Ex32;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,Math;
+
+var
+  CubDeNr,nr:double;
+  litera: char;
+begin
+  Write('Introduceti nr: ');
+  Readln(nr);
+  repeat
+    CubDeNr := Power(nr,3);
+    Writeln(floattostr(CubDeNr));
+    Writeln('Doriti sa continuati? d/D - DA');
+    Readln(litera);
+    nr := CubDeNr;
+  until ((litera<>'d') and (litera<>'D'));
+  Readln;
+end.
+
+ 
