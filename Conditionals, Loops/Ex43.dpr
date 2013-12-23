e15ebcb3b3fb6a7e87cb9f7bd1d9a0d63c918a91
Stroe Daniel <d.stroe@attosoft.ro>
2013-12-23 09:29:58 +0200

    no message

---
+++ Conditionals, Loops/Ex43.dpr
@@ -0,0 +1,28 @@
+program Ex43;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+var
+  an1, an2: integer;
+resourcestring
+  mesaj = 'Anul %d %s bisect';
+begin
+  Write('Introduceti primul an: ');
+  Readln(an1);
+  Write('Introduceti al doilea an: ');
+  Readln(an2);
+  if (an1 mod 400 = 0) then
+    Writeln(Format(mesaj, [1, 'este']))
+  else
+    Writeln(Format(mesaj, [1, 'nu este']));
+  if (an2 mod 400 = 0) then
+    Writeln(Format(mesaj, [2, 'este']))
+  else
+    Writeln(Format(mesaj, [2, 'nu este']));
+  Readln;
+end.
+
+ 
