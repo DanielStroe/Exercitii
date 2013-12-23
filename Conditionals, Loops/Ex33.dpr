ba42309ea7a428460feb79a1276ff2759ed93fce
Stroe Daniel <d.stroe@attosoft.ro>
2013-12-23 14:04:12 +0200

    no message

---
+++ Conditionals, Loops/Ex33.dpr
@@ -0,0 +1,57 @@
+program Ex33;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+var
+  n, i, j, aux: Integer;
+begin
+  Write('Introduceti n: ');
+  Readln(n);
+  for i := 1 to n do
+  begin
+    aux := 1;
+    for j := 1 to 2 * n - 1 do
+      if ((j >= n - i + 1) and (j <= n + i - 1)) then
+        if j < n then
+        begin
+          Write(inttostr(aux));
+          Inc(aux);
+        end
+        else
+          if j > n then
+          begin
+            Dec(aux);
+            Write(inttostr(aux));
+          end
+          else
+            Write(inttostr(i))
+        else Write(' ');
+          Writeln;
+  end;
+  for i := n - 1 downto 1 do
+  begin
+    aux := 1;
+    for j := 1 to 2 * n - 1 do
+      if ((j >= n - i + 1) and (j <= n + i - 1)) then
+        if j < n then
+        begin
+          Write(inttostr(aux));
+          Inc(aux);
+        end
+        else
+          if j > n then
+          begin
+            Dec(aux);
+            Write(inttostr(aux));
+          end
+          else
+            Write(inttostr(i))
+        else write(' ');
+          Writeln;
+  end;
+  Readln;
+end.
+
