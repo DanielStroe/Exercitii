3e16df9106b6a34494bbec9081bc91edad3490b6
Stroe Daniel <d.stroe@attosoft.ro>
2013-12-23 10:53:28 +0200

    no message

--- Conditionals, Loops/Ex45.dpr
+++ Conditionals, Loops/Ex45.dpr
@@ -14,12 +14,12 @@ var
 resourcestring
   mesaj = 'Populatia in anul %d era: %s';
 begin
-  Writeln(Format(mesaj, [1989, FloatToStr(Populatie1989)]));
+  Writeln(Format(mesaj, [1989, IntToStr(Populatie1989)]));
   PopulatieAn := Populatie1989;
   for i := 1990 to 2000 do
   begin
     PopulatieAn := PopulatieAn - (RataDeces * PopulatieAn) div 100 + (RataNastere * PopulatieAn) div 100;
-    Writeln(Format(mesaj, [i, FloatToStr(PopulatieAn)]));
+    Writeln(Format(mesaj, [i, IntToStr(PopulatieAn)]));
   end;
   Readln;
 end.
